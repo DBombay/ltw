a7a639b73aa7c26c0ea087a935dfe18ecab9dde0
Devin Bombay <dbombay@bdtrust.org>
2018-11-29 13:09:46 -0500

    Just adds another solution as examples

--- src/App/Game/lib/EventDeck.js
+++ src/App/Game/lib/EventDeck.js
@@ -6,70 +6,80 @@ export default {
       text: "What fact will help them over come not being a native speaker?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "no personal vehicle": {
       text: "What fact will help them over come not having a personal vehicle?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "welfare bias": {
       text: "What fact will help them over come having welfare bias?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "undocumented immigrant": {
       text: "What fact will help them over come not being a naturalized citizen?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "technical": {
       text: "What fact will address their technical barrier?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "physical disability": {
       text: "What fact will help them overcome their barrier present from their disability?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "knowledge lack": {
       text: "What facts will help fill their knowledge gap?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "previous eligibility": {
       text: "What will help them over come their past experience?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "no time": {
       text: "How can they MAKE TIME?!",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     },
     "previous experience": {
       text: "How can we help them to see that what's in the past is in the past?",
       solutions: [
         new Solution(1, 4, "I'm the right answer, pick me", "You picked the right one!", "Selected the right answer for non-native-speaker"),
-        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again.")
+        new Solution(2, 1, "I'm the wrong answer", "Nope. Try again."),
+        new Solution(3, 1, "I'm a misleading answer", "Here's an explanation why this answer was close, but not the best. Is there another answer that could fit better?")
       ]
     }
   }
